59c0797f15f70c63862ef01271b0cd507ab6d422
Choyongwoo95 <dpfmxlfls@gmail.com>
2021-03-16 16:31:59 +0900

    modify

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -15,11 +15,11 @@ function Main() {
 								<header id="header">
 									<a href="index.html" class="logo"><strong>Hawaiian-Pizza</strong> INVENTORY</a>
 									<ul class="icons">
-										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-										<li><a href="#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
-										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-										<li><a href="#" class="icon fa-medium"><span class="label">Medium</span></a></li>
+										<li><a href="!#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="!#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+										<li><a href="!#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+										<li><a href="!#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="!#" class="icon fa-medium"><span class="label">Medium</span></a></li>
 									</ul>
 								</header>
 						</div>
@@ -45,26 +45,26 @@ function Main() {
 										<li>
 											<span class="opener">Submenu</span>
 											<ul>
-												<li><a href="#">Lorem Dolor</a></li>
-												<li><a href="#">Ipsum Adipiscing</a></li>
-												<li><a href="#">Tempus Magna</a></li>
-												<li><a href="#">Feugiat Veroeros</a></li>
+												<li><a href="!#">Lorem Dolor</a></li>
+												<li><a href="!#">Ipsum Adipiscing</a></li>
+												<li><a href="!#">Tempus Magna</a></li>
+												<li><a href="!#">Feugiat Veroeros</a></li>
 											</ul>
 										</li>
-										<li><a href="#">Etiam Dolore</a></li>
-										<li><a href="#">Adipiscing</a></li>
+										<li><a href="!#">Etiam Dolore</a></li>
+										<li><a href="!#">Adipiscing</a></li>
 										<li>
 											<span class="opener">Another Submenu</span>
 											<ul>
-												<li><a href="#">Lorem Dolor</a></li>
-												<li><a href="#">Ipsum Adipiscing</a></li>
-												<li><a href="#">Tempus Magna</a></li>
-												<li><a href="#">Feugiat Veroeros</a></li>
+												<li><a href="!#">Lorem Dolor</a></li>
+												<li><a href="!#">Ipsum Adipiscing</a></li>
+												<li><a href="!#">Tempus Magna</a></li>
+												<li><a href="!#">Feugiat Veroeros</a></li>
 											</ul>
 										</li>
-										<li><a href="#">Maximus Erat</a></li>
-										<li><a href="#">Sapien Mauris</a></li>
-										<li><a href="#">Amet Lacinia</a></li>
+										<li><a href="!#">Maximus Erat</a></li>
+										<li><a href="!#">Sapien Mauris</a></li>
+										<li><a href="!#">Amet Lacinia</a></li>
 									</ul>
 								</nav>
 
@@ -75,7 +75,7 @@ function Main() {
 									</header>
 									<p>해당 사이트에 대하여 문의하고 싶다면, 다음 이메일로 연락주시길 바랍니다.</p>
 									<ul class="contact">
-										<li class="fa-envelope-o"><a href="#">dpfmxlfls95@naver.com</a></li>
+										<li class="fa-envelope-o"><a href="!#">dpfmxlfls95@naver.com</a></li>
 									</ul>
 								</section>
 

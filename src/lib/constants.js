3fa3009e27d1d0b7993c62eba22b98a15e3c53a0
seongwon <seongwon@edu.hanbat.ac.kr>
2021-05-06 12:20:51 +0900

    api url

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
-    BackUrl: 'https://inventory.hawaiian-pizza.gq',
+    BackUrl: 'https://inventory.hawaiian-pizza.pw',
   };
   
